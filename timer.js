73711e40b511dbca6fe6a3028ea2efbce74a0a34
Nevan Scott <nfscott@gmail.com>
2015-06-27 18:40:45 -0400

    click or spacebar to pause or resume

--- timer.js
+++ timer.js
@@ -28,6 +28,9 @@ function Timebox(timers) {
 Timebox.prototype.start = function(){
   this.timers[0].start();
 };
+Timebox.prototype.pause = function(){
+  this.timers[0].pause();
+};
 Timebox.prototype.addTimer = function(timer) {
   
 };
@@ -47,7 +50,7 @@ Timer.prototype.start = function(){
   this.step();
 };
 Timer.prototype.step = function(){
-  var that = this;
+  var timer = this;
   printTime(this.remaining, '#remaining');
   printTime(this.elapsed, '#elapsed');
   if(this.remaining > 0) {
@@ -56,18 +59,38 @@ Timer.prototype.step = function(){
     }
     this.elapsed++;
     this.remaining--;
-    this.t = setTimeout(function(){ that.step(); }, 1000);
+    this.t = setTimeout(function(){ timer.step(); }, 1000);
+    this.isPlaying = true;
   } else {
     $('body').attr('class', 'stop');
+    this.isPlaying = false;
   }
+};
+Timer.prototype.pause = function(){
+	if(this.isPlaying) {
+		this.isPlaying = false;
+		clearTimeout(this.t);
+	} else {
+		this.step();
+	}
 };
 
 var myTimebox = new Timebox(timers);
 myTimebox.start();
 
 $('.time').on('click', function(){
-	$('.time').removeClass('focus');
-	$(this).addClass('focus');
+	if($(this).hasClass('focus')) {
+		myTimebox.pause();
+	} else {
+		$('.time').removeClass('focus');
+		$(this).addClass('focus');
+	}
+});
+
+$(document).on('keypress', function(e){
+	if(e.keyCode === 32) {
+		myTimebox.pause();
+	}
 });
 
 function printTime(time, target) {
