2d9572d020699c6f28860226233936f61536feef
Nevan Scott <nfscott@gmail.com>
2015-08-22 20:46:37 -0400

    updating to use Backbone, hoping this will make future improvements easier

--- timer.js
+++ timer.js
@@ -1,98 +1,101 @@
-var timers = [
-  {
-    label: "Presentation",
-    minutes: 8,
-    warning: {
-      minutes: 2
-    },
-    countdown: {
-      seconds: 20
-    }
-  },
-  {
-    label: "Feedback",
-    minutes: 5,
-    warning: {
-      minutes: 1,
-    }
-  }
-];
+var Timer = Backbone.Model.extend({
+	defaults: function(){
+		return {
+			label: "Untitled",
+			duration: 60,
+		};
+	}
+});
 
-function Timebox(timers) {
-  this.timers = new Array();
-  var that = this;
-  timers.forEach(function(timer){
-    that.timers.push(new Timer(timer));
-  });
-}
-Timebox.prototype.start = function(){
-  this.timers[0].start();
-};
-Timebox.prototype.pause = function(){
-  this.timers[0].pause();
-};
-Timebox.prototype.addTimer = function(timer) {
-  
-};
+var Timebox = Backbone.Collection.extend({
+	model: Timer
+});
 
-function Timer(timer, timebox) {
-  this.timebox = timebox;
-  this.label = timer.label;
-  this.duration = timer.minutes * 60;
-  this.elapsed = 0;
-  this.remaining = this.duration;
-  if(timer.warning) {
-    this.warning = timer.warning.minutes * 60;
-  }
-}
-Timer.prototype.start = function(){
-  $('#label').text(this.label);
-  printTime(this.remaining, '#remaining');
-  printTime(this.elapsed, '#elapsed');
-};
-Timer.prototype.step = function(){
-  var timer = this;
-  printTime(this.remaining, '#remaining');
-  printTime(this.elapsed, '#elapsed');
-  if(this.remaining > 0) {
-    if(this.warning && this.remaining <= this.warning) {
-      $('body').attr('class', 'warning');
-    }
-    this.elapsed++;
-    this.remaining--;
-    this.t = setTimeout(function(){ timer.step(); }, 1000);
-    this.isPlaying = true;
-  } else {
-    $('body').attr('class', 'stop');
-    this.isPlaying = false;
-  }
-};
-Timer.prototype.pause = function(){
-	if(this.isPlaying) {
+var TimerView = Backbone.View.extend({
+	el: '#timer',
+	events: {
+		'click .time.focus': 'pause',
+		'click .time:not(.focus)': 'focus'
+	},
+	initialize: function(){
+		this.elapsed = 0;
+		this.remaining = this.model.get('duration');
 		this.isPlaying = false;
-		clearTimeout(this.t);
-	} else {
-		this.step();
-	}
-};
-
-var myTimebox = new Timebox(timers);
-myTimebox.start();
-
-$('.time').on('click', function(){
-	if($(this).hasClass('focus')) {
-		myTimebox.pause();
-	} else {
-		$('.time').removeClass('focus');
-		$(this).addClass('focus');
+		this.listenTo(this.model, 'change', this.render);
+	},
+	render: function(){
+		$('#label').text(this.model.get('label'));
+		printTime(this.remaining, '#remaining');
+		printTime(this.elapsed, '#elapsed');
+	},
+	step: function(){
+		var timerView = this;
+		printTime(this.remaining, '#remaining');
+		printTime(this.elapsed, '#elapsed');
+		if(this.remaining > 0) {
+			if(this.model.get('warning') && this.remaining <= this.model.get('warning')) {
+				$('body').attr('class', 'warning');
+			}
+			this.elapsed++;
+			this.remaining--;
+			this.t = setTimeout(function(){ timerView.step(); }, 1000);
+			this.isPlaying = true;
+		} else {
+			$('body').attr('class', 'stop');
+			this.isPlaying = false;
+		}
+	},
+	pause: function(){
+		if(this.isPlaying) {
+			this.isPlaying = false;
+			clearTimeout(this.t);
+		} else {
+			this.step();
+		}
+	},
+	focus: function(e){
+		this.$('.time').removeClass('focus');
+		$(e.currentTarget).addClass('focus');
 	}
 });
 
-$(document).on('keypress', function(e){
-	if(e.keyCode === 32) {
-		myTimebox.pause();
+var AppView = Backbone.View.extend({
+	el: document,
+	events: {
+		'keypress': 'pause'
+	},
+	render: function(){
+		this.activeTimer.render();
+	},
+	addTimer: function(timer){
+		this.activeTimer = new TimerView({model: timer});
+	},
+	pause: function(e){
+		if(e.keyCode === 32) {
+			this.activeTimer.pause();
+		}
 	}
 });
+
+$(function(){
+	var timer = new Timer({
+    label: "Presentation",
+    duration: 8*60,
+    warning: 2*60
+	});
+	var app = new AppView();
+	app.addTimer(timer);
+	app.render();
+});
+
+// $('#access').click(function(){
+// 	$('#timer').toggleClass('blur');
+// 	$('#controls').toggleClass('active');
+// });
+// 
+// $('#time_controls').submit(function(){
+// 	
+// });
 
 function printTime(time, target) {
   var minutes = Math.floor(time/60);
