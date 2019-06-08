0a86760a0140bb6dbb2d72205a82539f26b74a55
Nevan Scott <nfscott@gmail.com>
2019-06-08 16:53:19 +0700

    Use babel with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'main.js',
     format: 'umd'
-  }
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
 };
