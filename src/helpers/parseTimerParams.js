f8958842b11aa283b58a7f1c5183cd0c278722b1
Nevan Scott <nfscott@gmail.com>
2019-11-06 22:51:15 +0100

    Convert + to space from timer label param

--- src/helpers/parseTimerParams.js
+++ src/helpers/parseTimerParams.js
@@ -4,7 +4,7 @@ export function parseTimerParams(str=window.location.search) {
   return str.includes('?t=') && str.split('?t=')[1].split('|').map(
     (part) => {
       return {
-        label: part.split(':')[0],
+        label: part.split(':')[0].replace('+', ' '),
         duration: timeInSeconds(part.split(':')[1])
       }
     }
