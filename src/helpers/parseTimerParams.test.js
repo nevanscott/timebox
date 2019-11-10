a7cf988a71b9a6a36a42a77fedbd5ad4eabbc321
Nevan Scott <nfscott@gmail.com>
2019-11-10 12:22:26 +0100

    Test for special character encoding support

--- src/helpers/parseTimerParams.test.js
+++ src/helpers/parseTimerParams.test.js
@@ -1,11 +1,26 @@
 const { parseTimerParams } = require('./parseTimerParams');
 
-test('?t=Decide:34', () => {
+test('parse a single timer', () => {
   const q = '?t=Decide:34';
   const result = [
     {
       label: 'Decide',
       duration: 34*60
+    }
+  ]
+  expect(parseTimerParams(q)).toStrictEqual(result);
+});
+
+test('handle special characters', () => {
+  const q = '?t=Presentation:15%7CFeedback:2';
+  const result = [
+    {
+      label: 'Presentation',
+      duration: 15*60
+    },
+    {
+      label: 'Feedback',
+      duration: 2*60
     }
   ]
   expect(parseTimerParams(q)).toStrictEqual(result);
