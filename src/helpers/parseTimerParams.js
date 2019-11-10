abf618b0a17018c59fff4b513ed7010a77c88011
Nevan Scott <nfscott@gmail.com>
2019-11-10 12:20:03 +0100

    Decode special characters in URLs

--- src/helpers/parseTimerParams.js
+++ src/helpers/parseTimerParams.js
@@ -1,7 +1,7 @@
 import { timeInSeconds } from './timeInSeconds';
 
 export function parseTimerParams(str=window.location.search) {
-  return str.includes('?t=') && str.split('?t=')[1].split('|').map(
+  return str.includes('?t=') && decodeURIComponent(str.split('?t=')[1]).split('|').map(
     (part) => {
       return {
         label: part.split(':')[0].replace('+', ' '),
