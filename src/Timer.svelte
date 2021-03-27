20ee2fa997d5b363c176fef55bad5be982097278
Nevan Scott <nfscott@gmail.com>
2021-03-27 11:43:53 +0100

    remove button tweaks

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -130,7 +130,11 @@
     <button
       class="remove action"
       on:click|stopPropagation={remove}
-    >&times;</button>
+    >
+      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M5.24408 5.24408C5.56952 4.91864 6.09715 4.91864 6.42259 5.24408L10 8.8215L13.5774 5.24408C13.9029 4.91864 14.4305 4.91864 14.7559 5.24408C15.0814 5.56952 15.0814 6.09715 14.7559 6.42259L11.1785 10L14.7559 13.5774C15.0814 13.9029 15.0814 14.4305 14.7559 14.7559C14.4305 15.0814 13.9029 15.0814 13.5774 14.7559L10 11.1785L6.42259 14.7559C6.09715 15.0814 5.56952 15.0814 5.24408 14.7559C4.91864 14.4305 4.91864 13.9029 5.24408 13.5774L8.8215 10L5.24408 6.42259C4.91864 6.09715 4.91864 5.56952 5.24408 5.24408Z" fill="currentColor"/>
+      </svg>
+    </button>
   {:else if !playing}
     <button
       class="reset action"
@@ -176,10 +180,11 @@
 }
 
 .remove {
-  height: 1.25em;
-  width: 1.25em;
-  font-size: 1.2em;
-  line-height: 1;
+  height: 24px;
+  width: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .reset {
   font-size: smaller;
